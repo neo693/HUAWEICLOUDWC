274c057820c1f2c2f19c1fe167386629da595615
kimixu717 <kimixu0717@gmail.com>
2018-07-08 20:27:58 +0800

    修改命名

--- src/views/Question/Question.vue
+++ src/views/Question/Question.vue
@@ -1,25 +1,32 @@
 <template>
-  <div>
-    <img :src="imgSrc" alt="">
-  </div>
+    <div class="question-wrap">
+      <QuestionBanner :selected="selected"></QuestionBanner>
+      <QuestionTitle></QuestionTitle>
+      <QuestionFooter @select="select"></QuestionFooter>
+    </div>
 </template>
 
 <script>
+  import QuestionBanner from './QuestionBanner'
+  import QuestionTitle from './QuestionTitle'
+  import QuestionFooter from './QuestionFooter'
+
   export default {
     name: "Question",
-    computed: {
-      imgSrc() {
-        return `/static/imgs/Question/${this.$route.name}.png`
+    components: { QuestionBanner, QuestionTitle, QuestionFooter },
+    data() {
+      return {
+        selected: false
+      }
+    },
+    methods: {
+      select(answer) {
+        this.selected = answer
       }
     }
   }
 </script>
 
-<style lang="scss" scoped>
-  img {
-    position: fixed;
-    top: 293px;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+<style scoped>
+
 </style>
---
+++ src/views/Question/QuestionTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img :src="imgSrc" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "QuestionTitle",
+    computed: {
+      imgSrc() {
+        return `/static/imgs/Question/${this.$route.name}.png`
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  img {
+    position: fixed;
+    top: 293px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>
