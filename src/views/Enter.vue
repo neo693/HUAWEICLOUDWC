7a27a3c4f327a3e412893c6653bfd3ddc1654fad
simons <738739100@qq.com>
2018-07-10 00:09:13 +0800

    anime

--- src/views/Enter.vue
+++ src/views/Enter.vue
@@ -1,24 +1,108 @@
 <template>
   <div class="enter">
     <img src="/static/imgs/标题@2x.png" alt="title" class="title">
-    <img src="/static/imgs/群組1@2x.png" alt="man" class="man1 animated slideInLeft" >
+   <!-- <img src="/static/imgs/群組1@2x.png" alt="man" class="man1 animated slideInLeft" >-->
+
+    <img src="/static/imgs/enter/head.png" class="man2" v-show="show_head_ball">
+    <img src="/static/imgs/enter/ball1.png" class="ball1" v-show="show_head_ball">
+    <img src="/static/imgs/enter/gate.png" class="gate" v-show="show_gate">
+    <img src="/static/imgs/enter/hit.png" class="hit" v-show="show_hit">
+    <img src="/static/imgs/enter/ball2.png" class="ball2" v-show="show_hit">
     <img src="/static/imgs/开启测试之旅@2x.png" alt="start" class="start animated fadeIn" @click="start" v-show="show_btn">
-    <img src="/static/imgs/打开声音@2x.png" alt="music" class="music animated fadeIn" v-show="show_btn">
+    <img :src="music_url" alt="music" class="music animated fadeIn" v-show="show_btn" @click="switchMusic">
+
   </div>
 </template>
 <script>
   export default {
     data(){
       return {
-        show_btn:false
+        show_btn:false,
+        show_head_ball:true,
+        show_gate:false,
+        show_hit:false,
+        music_url:'/static/imgs/打开声音@2x.png'
       }
     },
     methods:{
       start(){
         this.$router.push({name:'Character'})
+      },
+      animate(){
+       this.show_head_ball=true
+        this.show_gate=false
+        this.show_hit=false
+
+        let ball2=anime({
+          targets: '.ball1',
+          translateX:[-200,0],
+          translateY: [-100,0],
+          duration: 2000,
+          loop: false,
+          easing: 'easeInCubic'
+        })
+        let man2=anime({
+          targets: '.man2',
+          translateX:[275,0],
+          translateY: [567,0],
+          duration: 2000,
+          loop: false,
+          easing: 'easeInCubic'
+        })
+        man2.finished.then(()=>{
+          setTimeout(()=>{
+            this.show_head_ball=false
+            this.show_gate=true
+            let man3=anime({
+              targets: '.gate',
+              translateX:[-375,0],
+              duration: 2000,
+              loop: false,
+              easing: 'easeInOutQuart'
+            })
+            man3.finished.then(()=>{
+              setTimeout(()=>{
+                this.show_gate=false
+                this.show_hit = true
+                let man1=anime({
+                  targets: '.hit',
+                  translateX:[475,0],
+                  translateY: [767,0],
+                  duration: 2000,
+                  loop: false,
+                  easing: 'easeInCubic'
+                })
+                let ball2=anime(
+                  {
+                    targets: '.ball2',
+                    translateX:[475,0],
+                    translateY: [50,0],
+                    duration: 2000,
+                    loop: false,
+                    easing: 'easeInCubic'
+                  }
+                )
+                ball2.finished.then(()=>{
+                  setTimeout(()=>{
+                    this.animate()
+                  },1000)
+
+                })
+              },1000)
+            })
+          },1000)
+        });
+      },
+      switchMusic(){
+         if(this.music_url=='/static/imgs/打开声音@2x.png'){
+           this.music_url='/static/imgs/enter/框@2x.png'
+         }else if(this.music_url=='/static/imgs/enter/框@2x.png'){
+           this.music_url='/static/imgs/打开声音@2x.png'
+         }
       }
     },
     mounted(){
+      this.animate()
       setTimeout(()=>{
         this.show_btn=true
       },1000)
@@ -69,6 +153,46 @@
         right: 0;
         width: 118px;
         height: 23px;
+      }
+      .man2{
+        position: absolute;
+        display: block;
+        width: 375px;
+        left:0;
+        right: 0;
+        margin: auto;
+      }
+      .ball1{
+        position: absolute;
+        display: block;
+        width: 375px;
+        left:0;
+        right: 0;
+        margin: auto;
+      }
+      .gate{
+        position: absolute;
+        display: block;
+        width: 375px;
+        left:0;
+        right: 0;
+        margin: auto;
+      }
+      .hit{
+        position: absolute;
+        display: block;
+        width: 375px;
+        left:0;
+        right: 0;
+        margin: auto;
+      }
+      .ball2{
+        position: absolute;
+        display: block;
+        width: 375px;
+        left:0;
+        right: 0;
+        margin: auto;
       }
     }
 </style>
