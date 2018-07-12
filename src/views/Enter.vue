b114fb47884cc56ce36efe7c62d7f789f5670b9c
徐玉金 <xuyujin@banggood.com>
2018-07-12 15:14:22 +0800

    修改进入页的动画

--- src/views/Enter.vue
+++ src/views/Enter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="enter">
     <img src="/static/imgs/标题@2x.png" alt="title" class="title">
-   <!-- <img src="/static/imgs/群組1@2x.png" alt="man" class="man1 animated slideInLeft" >-->
+    <!-- <img src="/static/imgs/群組1@2x.png" alt="man" class="man1 animated slideInLeft" >-->
 
     <img src="/static/imgs/enter/head.png" class="man2" v-show="show_head_ball">
     <img src="/static/imgs/enter/ball11.png" class="ball1" v-show="show_head_ball">
@@ -18,17 +18,17 @@
       return {
         show_btn:false,
         show_head_ball:true,
-        show_gate:false,
-        show_hit:false,
+        show_gate:true,
+        show_hit:true,
         music_url:'/static/imgs/打开声音@2x.png',
-        time_dur:1000,
+        time_dur:1500,
         time_delay:600,
         ball1:'',
         man2:'',
         ball2:'',
         timer1:'',
         timer2:'',
-        timer3:''
+        timer3:'',
       }
     },
     methods:{
@@ -36,193 +36,186 @@
         this.$router.push({name:'Character'})
       },
       animate(){
-        let that=this
-        this.ball1=anime({
-          targets: '.ball1',
-          translateX:[-200,0],
-          translateY: [-100,0],
-          duration: that.time_dur,
-          rotate:'3turn',
-          loop: false,
-          easing: 'linear'
-        })
-         this.man2=anime({
-          targets: '.man2',
-          translateX:[275,0],
-          translateY: [467,0],
-          duration: that.time_dur,
-          loop: false,
-          easing: 'linear'
-        })
-        this.man2.finished.then(()=>{
-          setTimeout(()=>{
-            this.show_head_ball=false
-            this.show_gate=true
-            let man3=anime({
-              targets: '.gate',
-              translateX:[-375,0],
-              duration: that.time_dur,
-              loop: false,
-              easing: 'linear'
-            })
-            man3.finished.then(()=>{
-              setTimeout(()=>{
-                this.show_gate=false
-                this.show_hit = true
-                this.ball2.restart()
-                let man1=anime({
-                  targets: '.hit',
-                  translateX:[275,0],
-                  translateY: [467,0],
-                  duration: that.time_dur,
-                  loop: false,
-                  easing: 'linear'
-                })
-          
-                 this.ball2=anime(
-                  {
-                    targets: '.ball2',
-                    translateX:[475,0],
-                    translateY: [50,0],
-                    duration: that.time_dur,
-                    rotate:'3turn',
-                    loop: false,
-                    easing: 'linear',
-            
-                  }
-                )
-            
-                this.ball2.finished.then(()=>{
-                  setTimeout(()=>{
-                    that.show_gate=false
-                    that.show_hit=false
-                    that.show_head_ball=true
-                    //that.ball1.restart()
-                    //that.animate()
-                  },that.time_delay)
-                })
-              },that.time_delay)
-            })
-          },that.time_delay)
+        var manTimeline = anime.timeline({
+          loop: true
         });
+        manTimeline
+          .add({
+            targets: '.man2',
+            translateX:[0,-400, -400],
+            translateY: [0,-230, -230],
+            duration: this.time_dur,
+            easing: 'linear'
+          })
+          .add({
+            targets: '.gate',
+            translateX:[0,375,375],
+            translateY:[0,60,60],
+            duration: this.time_dur,
+            easing: 'linear'
+          })
+          .add({
+            targets: '.hit',
+            translateX:[0,-360,-360],
+            translateY: [0,-400,-400],
+            duration: this.time_dur,
+            easing: 'linear'
+          })
+
+        var ballTimeline = anime.timeline({
+          loop: true
+        })
+        ballTimeline
+          .add({
+            targets: '.ball1',
+            translateX:[0,130,130],
+            translateY: [0,-100,-100],
+            duration: this.time_dur,
+            rotate:'3turn',
+            easing: 'linear'
+          })
+          .add({
+            targets: '.ball2',
+            translateX:[0,-350,-350],
+            translateY: [0,100,100],
+            duration: this.time_dur,
+            rotate:'3turn',
+            easing: 'linear',
+            delay: this.time_dur
+          })
+
+        var hiddenTimeline = anime.timeline({
+          loop: true
+        })
+        hiddenTimeline
+          .add({
+            targets: ['.man2, .ball1'],
+            duration: 1,
+            opacity: 0,
+            easing: 'linear',
+            delay: this.time_dur
+          })
+          .add({
+            targets: '.gate',
+            duration: 1,
+            opacity: 0,
+            easing: 'linear',
+            delay: this.time_dur
+          })
+          .add({
+            targets: ['.hit', '.ball2'],
+            duration: 1,
+            opacity: 0,
+            easing: 'linear',
+            delay: this.time_dur
+          })
+
+
       },
       switchMusic(){
-         if(this.music_url=='/static/imgs/打开声音@2x.png'){
-           this.music_url='/static/imgs/enter/框@2x.png'
-           document.getElementById('audio').pause()
-         }else if(this.music_url=='/static/imgs/enter/框@2x.png'){
-           this.music_url='/static/imgs/打开声音@2x.png'
-           document.getElementById('audio').play()
-         }
+        if(this.music_url=='/static/imgs/打开声音@2x.png'){
+          this.music_url='/static/imgs/enter/框@2x.png'
+          document.getElementById('audio').pause()
+        }else if(this.music_url=='/static/imgs/enter/框@2x.png'){
+          this.music_url='/static/imgs/打开声音@2x.png'
+          document.getElementById('audio').play()
+        }
       },
     },
     mounted(){
       document.getElementById('audio').play()
       this.animate()
-      let time1=this.time_dur*3+this.time_delay*3
-      let time2=this.time_dur*2+this.time_delay*2
 
-       this.timer1=window.setInterval(()=>{
-        this.ball1.restart()
-        this.animate()
-        this.timer2=window.setTimeout(()=>{
-          this.ball2.restart()
-        },time2)
-      },time1)
-      setTimeout(()=>{
-        this.show_btn=true
-      },time1)
-    },
-    beforeDestroy(){
-       window.clearInterval(this.timer1)
-       window.clearTimeout(this.timer2)
+      setTimeout(() => {
+        this.show_btn = true
+      }, 3*this.time_dur)
     }
   }
 </script>
 <style scoped lang="scss">
-    .enter{
-      width:100%;
-      height: 100%;
-      background: url('/static/imgs/line2@2x.png') no-repeat center 154px;
-      position: relative;
-      overflow: hidden;
-      .title{
-        display: block;
-        position:absolute;
-        top:12px;
-        margin: auto;
-        left:0;
-        right: 0;
-        width: 245px;
-        height: 160px;
-      }
-      .man1{
-        position: absolute;
-        display: block;
-        top:182px;
-        width: 360px;
-        height: 340px;
-        left:0;
-        right: 0;
-        margin: auto;
-      }
-      .start{
-        position: absolute;
-        display: block;
-        bottom:31px;
-        margin: auto;
-        left:0;
-        right: 0;
-        width: 184px;
-        height: 68px;
-      }
-      .music{
-        position: absolute;
-        display: block;
-        bottom:11px;
-        margin: auto;
-        left:0;
-        right: 0;
-        width: 118px;
-        height: 23px;
-      }
-      .man2{
-        position: absolute;
-        display: block;
-        width: 375px;
-        left:0;
-        right: 0;
-        margin: auto;
-      }
-      .ball1{
-        position: absolute;
-        display: block;
-        width: 75px;
-        top:142px;
-        left:0;
-      }
-      .gate{
-        position: absolute;
-        display: block;
-        width: 375px;
-        left:0;
-        right: 0;
-        margin: auto;
-      }
-      .hit{
-        position: absolute;
-        display: block;
-        width: 375px;
-        left:0;
-        right: 0;
-        margin: auto;
-      }
-      .ball2{
-        position: absolute;
-        display: block;
-        width: 61px;
-        top:122px;
-        left:90px;
-      }
+  .enter{
+    width:100%;
+    height: 100%;
+    background: url('/static/imgs/line2@2x.png') no-repeat center 154px;
+    position: relative;
+    overflow: hidden;
+    .title{
+      display: block;
+      position:absolute;
+      top:12px;
+      margin: auto;
+      left:0;
+      right: 0;
+      width: 245px;
+      height: 160px;
     }
+    .man1{
+      position: absolute;
+      display: block;
+      top:182px;
+      width: 360px;
+      height: 340px;
+      left:0;
+      right: 0;
+      margin: auto;
+    }
+    .start{
+      position: absolute;
+      display: block;
+      bottom:31px;
+      margin: auto;
+      left:0;
+      right: 0;
+      width: 184px;
+      height: 68px;
+    }
+    .music{
+      position: absolute;
+      display: block;
+      bottom:11px;
+      margin: auto;
+      left:0;
+      right: 0;
+      width: 118px;
+      height: 23px;
+    }
+    .man2{
+      position: absolute;
+      display: block;
+      width: 375px;
+      right:-400px;
+      bottom: -200px;
+      margin: auto;
+    }
+    .ball1{
+      position: absolute;
+      display: block;
+      width: 75px;
+      top:240px;
+      left:-120px;
+    }
+    .gate{
+      position: absolute;
+      display: block;
+      width: 375px;
+      left:-720px;
+      right: 0;
+      margin: auto;
+    }
+    .hit{
+      position: absolute;
+      display: block;
+      width: 375px;
+      right:-325px;
+      bottom: -452px;
+      margin: auto;
+    }
+    .ball2{
+      position: absolute;
+      display: block;
+      width: 61px;
+      top:40px;
+      right:-80px;
+    }
+  }
 </style>
